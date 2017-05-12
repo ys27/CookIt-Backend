96e439aec25d606dd43ccbe2c7d94635ef2ac019
Jeong Won An <jw3an@uwaterloo.ca>
2017-05-12 16:10:42 -0400

    Added schema files

---
+++ schema/user.js
@@ -0,0 +1,10 @@
+{
+	"fistName": String,
+	"lastName": String,
+	"email": String,
+	"password": String,
+	"birthDay": String,
+	"allergies", [String],
+	"favouriteIngredients": [String],
+	"recipes": [String]
+}
