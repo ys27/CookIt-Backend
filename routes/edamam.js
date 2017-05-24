147653d79d1b93d56d4ffab86823ab42eb3041c9
Bill Huang <billhuang@live.ca>
2017-05-24 02:56:51 -0400

    Minor change to find popular endpoint

--- routes/edamam.js
+++ routes/edamam.js
@@ -13,8 +13,8 @@ var app_key = '6d76b47efaeb85aba24ff109d9d82982';
 var host = `https://api.edamam.com/search?app_id=${app_id}&app_key=${app_key}`;
 
 //Get 20 Popular RecipesQQQQ
-router.get('/find/popular', function(req, res, next) {
-	var keywords = "&q=popular";
+router.get('/find/popular/:id', function(req, res, next) {
+	var keywords = `&q=${req.params.id}`;
 	var limit = "&from=0&to=20";
 	request({
 		uri: host + keywords + limit,
@@ -32,9 +32,6 @@ router.get('/find/popular', function(req, res, next) {
 					healthLabels: recipeItem.recipe.healthLabels,
 				});
 		});
-
-		console.log(bodyJSON);
-
 		res.send(JSON.stringify(responseContainer));
 	})
 });
